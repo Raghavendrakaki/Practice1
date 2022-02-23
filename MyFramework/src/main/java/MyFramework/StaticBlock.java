d4c6c5329a3b75c059ec40c54e83287bb723d036
Raghavendra <raghavamyl@gmail.com>
2022-02-23 14:40:51 +0530

    main methods commented

--- MyFramework/src/main/java/MyFramework/StaticBlock.java
+++ MyFramework/src/main/java/MyFramework/StaticBlock.java
@@ -4,25 +4,35 @@ public class StaticBlock {
 	static {
 		System.out.println("my name is Raghavendra");
 	}
-public static void main(String[] args) {
-	
-	System.out.println("This is in main method");
-	System.out.println("This is in main method");
-	System.out.println("This is in main method");
-	System.out.println("This is in main method");System.out.println("This is in main method");
-	System.out.println("This is in main method");System.out.println("This is in main method");
-	System.out.println("This is in main method");System.out.println("This is in main method");
-	System.out.println("This is in main method");System.out.println("This is in main method");
-	System.out.println("This is in main method");
-	System.out.println("This is in main method");
-	System.out.println("This is in main method");
-	System.out.println("This is in main method");System.out.println("This is in main method");
-	System.out.println("This is in main method");System.out.println("This is in main method");
-	System.out.println("This is in main method");System.out.println("This is in main method");
-	System.out.println("This is in main method");System.out.println("This is in main method");
-	System.out.println("This is in main method");
-	System.out.println("This is in main method");
-	System.out.println("Test Branch updated");
-	
-}
+	/*
+	 * public static void main(String[] args) {
+	 * 
+	 * System.out.println("This is in main method");
+	 * System.out.println("This is in main method");
+	 * System.out.println("This is in main method");
+	 * System.out.println("This is in main method");System.out.
+	 * println("This is in main method");
+	 * System.out.println("This is in main method");System.out.
+	 * println("This is in main method");
+	 * System.out.println("This is in main method");System.out.
+	 * println("This is in main method");
+	 * System.out.println("This is in main method");System.out.
+	 * println("This is in main method");
+	 * System.out.println("This is in main method");
+	 * System.out.println("This is in main method");
+	 * System.out.println("This is in main method");
+	 * System.out.println("This is in main method");System.out.
+	 * println("This is in main method");
+	 * System.out.println("This is in main method");System.out.
+	 * println("This is in main method");
+	 * System.out.println("This is in main method");System.out.
+	 * println("This is in main method");
+	 * System.out.println("This is in main method");System.out.
+	 * println("This is in main method");
+	 * System.out.println("This is in main method");
+	 * System.out.println("This is in main method");
+	 * System.out.println("Test Branch updated");
+	 * 
+	 * }
+	 */
 }
